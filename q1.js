2ba86337bbebd8f1df21adf1244fb387355716e5
Matan <matansabag202@gmail.com>
2014-03-18 13:40:25 +0200

    Second

--- q1.js
+++ q1.js
@@ -3,8 +3,8 @@
 */
 
 function fibbonachi (n) {
-	if (n==0) {return 0};
-	if (n==1) {return 1};
+	if (n==0) {return 0;}
+	if (n==1) {return 1;}
 	return fibbonachi(n-1)+fibbonachi(n-2);
 }
  var num=20
